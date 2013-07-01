5859c5a45c2fda7cab5d81319946824b631aff9f
Nguyen Vinh Thinh <nvthinh.hut.hedspi@gmail.com>
2013-07-01 10:38:06 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DailyReportSystem::Application.config.secret_token = '534b41f9cf19d46f3518c91271afc4039777efdc36e21b43e8bae4f71b3e0677bd3d1d05a12155862dd5025fd06f89ec337f215069dc1b89bbb3e6eab6447937'
